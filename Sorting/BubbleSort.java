814fd09df23d2430a75d36952e56a4256308bff0
Rishabh <92112986+rishabhyadav23@users.noreply.github.com>
2025-07-27 18:14:02 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,8 +1,12 @@
 package Sorting;
-
 // Bubble Sort
+
+// Repeatedly compares adjacent elements and swaps them if they are in the wrong order.
+// Process continues until the array is sorted.
+
 // Time Complexity - O(n^2) | best - O(n)
 // Space Complexity - O(1);
+// Stable: Yes
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = { 3, 70, 3, 9, 5, 2, 8, 6, 40, 42, 1 };
